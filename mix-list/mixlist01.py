04b76807a7e74c43d320c764771bc3123e4ab188
prabhupitchai <prabhupitchai@users.noreply.github.com>
2022-07-11 20:31:11 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# create a list containing three items
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+#dispaly first 3 items in the list
+print("The first item in the list (IP): " + my_list[0] )
+
+#display the 2nd item on the list
+print("The last item in the list (state): " + my_list[2] )
+
+#display 3rd item on the list
+print("The last item in the lust(state): "+my_list[2])
+
+#Create the IP List
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+#display only the IP address
+print("The IP List from the above array is:",iplist[3],",and",iplist[4])
